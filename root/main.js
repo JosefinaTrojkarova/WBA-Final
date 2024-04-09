d9644ef5bf3bb3188a97f3cf55101cae55cbb6c1
JosefinaTrojkarova <trojkarjosef@gmail.com>
2024-04-10 00:09:41 +0200

    Added working three js - only early function

--- root/main.js
+++ root/main.js
@@ -1,9 +1,50 @@
-document.querySelector('#nav-logo').addEventListener('click', function(e) {
-    e.preventDefault(); // Prevent the default link behavior
-  
-    window.scrollTo({
-      top: 0,
-      behavior: 'smooth' // Enable smooth scrolling
-    });
-  });
-  
+import * as THREE from 'three';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
+import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader.js';
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
+const light = new THREE.PointLight(0xffffff, 10, 1000);
+light.position.set(0, 0, 5);
+scene.add(light);
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );
+
+const loader = new GLTFLoader();
+
+// Optional: Provide a DRACOLoader instance to decode compressed mesh data
+const dracoLoader = new DRACOLoader();
+dracoLoader.setDecoderPath( 'https://www.gstatic.com/draco/versioned/decoders/1.5.7/' );
+dracoLoader.setDecoderConfig( { type: 'js' } );
+loader.setDRACOLoader( dracoLoader );
+
+// Load a glTF resource
+loader.load('models/wavetag.glb', function ( gltf ) {
+
+		scene.add( gltf.scene );
+
+	},
+	// called while loading is progressing
+	function ( xhr ) {
+
+		console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
+
+	},
+	// called when loading has errors
+	function ( error ) {
+
+		console.log( 'An error happened' );
+
+	}
+);
+
+camera.position.z = 5;
+
+function animate() {
+	requestAnimationFrame( animate );
+	renderer.render( scene, camera );
+}
+animate();
