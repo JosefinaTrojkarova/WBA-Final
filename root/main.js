130f54829a4e35f857d41e789ccc3a53654b40b1
JosefinaTrojkarova <trojkarjosef@gmail.com>
2024-05-01 09:16:18 +0200

    Hero model adjustments
    - Rotation and orbit controls added and debugged
    - Lighting fixed

--- root/main.js
+++ root/main.js
@@ -1,38 +1,41 @@
 import * as THREE from 'three';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader.js';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import { gsap } from "gsap";
+import { MotionPathPlugin } from "gsap/MotionPathPlugin.js";
 
 // Create scene and camera
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
 // Add lights
-const ambientLight = new THREE.AmbientLight( 0x404040 );
-scene.add( ambientLight );
-
 const light1 = new THREE.PointLight( 0xffffff, 200, 1000 );
-light1.position.set( 5, 0, 6 );
-scene.add( light1 );
+light1.position.set( 5, 0, -1 );
 
 const light2 = new THREE.PointLight( 0xffffff, 200, 1000 );
-light2.position.set( -5, 0, 6 );
-scene.add( light2 );
+light2.position.set( -5, 0, -1 );
 
 const light3 = new THREE.PointLight( 0xffffff, 100, 3000 );
-light3.position.set( 8, 0, 3 );
-scene.add( light3 );
+light3.position.set( 8, 0, -4 );
 
 const light4 = new THREE.PointLight( 0xffffff, 100, 3000 );
-light4.position.set( -8, 0, 3 );
-scene.add( light4 );
+light4.position.set( -8, 0, -4 );
 
 const blueLight1 = new THREE.PointLight( 0x3399FF, 400, 2000 );
-blueLight1.position.set( 5, 0, -5 );
-scene.add( blueLight1 );
+blueLight1.position.set( 5, 0, -12 );
 
 const blueLight2 = new THREE.PointLight( 0x3366cc, 400, 2000 );
-blueLight2.position.set( -5, 0, -5 );
-scene.add( blueLight2 ); 
+blueLight2.position.set( -5, 0, -12 );
+
+camera.add(light1);
+camera.add(light2);
+camera.add(light3);
+camera.add(light4);
+camera.add(blueLight1);
+camera.add(blueLight2);
+
+scene.add(camera);
 
 // Create renderer
 const renderer = new THREE.WebGLRenderer({ alpha: true });
@@ -43,6 +46,81 @@ document.body.appendChild( renderer.domElement );
 // Append the renderer to a section in the HTML file
 const section = document.getElementById( "hero-model" );
 section.appendChild( renderer.domElement );
+
+// Create controls
+const controls = new OrbitControls( camera, renderer.domElement );
+controls.enablePan = false;
+controls.enableZoom = false;
+camera.position.z = 7;
+controls.update();
+
+let animationRunning = false;
+gsap.registerPlugin(MotionPathPlugin) 
+
+function startAnimation() {
+	let currentX = camera.position.x;
+	let currentZ = camera.position.z;
+	let currentY = camera.position.y;
+
+	// Calculate the current distance from the center
+	let currentDistance = Math.sqrt(currentX * currentX + currentZ * currentZ);
+
+	// Calculate the scaling factor to maintain a distance of 7 units from the center
+	let scaleFactor = 7 / currentDistance;
+
+	// Calculate the new x and z positions to maintain the distance of 7 from the center
+	let newX = currentX * scaleFactor;
+	let newZ = currentZ * scaleFactor;
+
+	if (!animationRunning) {
+		animationRunning = true;
+		gsap.to(camera.position, { 
+    		duration: 3, 
+			motionPath: {
+				path: [
+					{x:currentX, y:currentY, z:currentZ}, 
+					{x:currentX + (newX - currentX) / 2, y:currentY, z:currentZ + (newZ - currentZ) / 2}, 
+					{x:newX, y:currentY/2, z:newZ}, 
+					{x: newX, y: 0, z: newZ}],
+				type: "cubic"
+			  },
+    		ease: "power2.inOut", 
+    		onComplete: () => {
+        		if (animationRunning) {
+            		startAnimation();
+        		}
+    		}
+		});
+	}
+}
+
+function stopAnimation() {
+	animationRunning = false;
+	gsap.killTweensOf(camera.position); // Stop the animation
+}
+
+const modelRotationZ= -0.003;
+
+// Listen for console commands
+let animationStartTimeout;
+let isCountingDown = false;
+
+controls.addEventListener('start', function () {
+    stopAnimation();
+    if (isCountingDown) {
+        clearTimeout(animationStartTimeout);
+        isCountingDown = false;
+    }
+});
+
+controls.addEventListener('end', function () {
+    if (!isCountingDown) {
+		isCountingDown = true;
+        animationStartTimeout = setTimeout(startAnimation, 1000);
+        modelRotationZ = -0.003;
+    }
+});
+
 
 // glTF loader
 const loader = new GLTFLoader();
@@ -77,15 +155,16 @@ loader.load('models/wavetag.glb', function ( gltf ) {
 	}
 );
 
-// Set camera position
-camera.position.z = 7;
-
-// Add rotation animation
+// Animation loop
 function animate() {
-	requestAnimationFrame( animate );
+    requestAnimationFrame(animate);
 
-    model.rotation.z += -0.005;
+	controls.enableDamping = true;
+    controls.update();
 
-	renderer.render( scene, camera );
+	model.rotation.z += modelRotationZ;
+
+    renderer.render(scene, camera);
 }
+
 animate();
---
+++ root/other.js
@@ -0,0 +1,8 @@
+document.querySelector('#nav-logo').addEventListener('click', function(e) {
+    e.preventDefault(); // Prevent the default link behavior
+  
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth' // Enable smooth scrolling
+    });
+  });
