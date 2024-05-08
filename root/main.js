02516574b7dc763231aa3d11e3405c0fa85654a7
JosefinaTrojkarova <trojkarjosef@gmail.com>
2024-05-08 19:23:13 +0200

    Major addons to the main page
    - Added sub-hero section
    - Added how-it-works section
    - Added why-us header
    - Added parallax effects
    - Code cleaned up

--- root/main.js
+++ root/main.js
@@ -7,26 +7,26 @@ import { MotionPathPlugin } from "gsap/MotionPathPlugin.js";
 
 // Create scene and camera
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
 // Add lights
-const light1 = new THREE.PointLight( 0xffffff, 200, 1000 );
-light1.position.set( 5, 0, -1 );
+const light1 = new THREE.PointLight(0xffffff, 200, 1000);
+light1.position.set(5, 0, -1);
 
-const light2 = new THREE.PointLight( 0xffffff, 200, 1000 );
-light2.position.set( -5, 0, -1 );
+const light2 = new THREE.PointLight(0xffffff, 200, 1000);
+light2.position.set(-5, 0, -1);
 
-const light3 = new THREE.PointLight( 0xffffff, 100, 3000 );
-light3.position.set( 8, 0, -4 );
+const light3 = new THREE.PointLight(0xffffff, 100, 3000);
+light3.position.set(8, 0, -4);
 
-const light4 = new THREE.PointLight( 0xffffff, 100, 3000 );
-light4.position.set( -8, 0, -4 );
+const light4 = new THREE.PointLight(0xffffff, 100, 3000);
+light4.position.set(-8, 0, -4);
 
-const blueLight1 = new THREE.PointLight( 0x3399FF, 400, 2000 );
-blueLight1.position.set( 5, 0, -12 );
+const blueLight1 = new THREE.PointLight(0x3399FF, 400, 2000);
+blueLight1.position.set(5, 0, -12);
 
-const blueLight2 = new THREE.PointLight( 0x3366cc, 400, 2000 );
-blueLight2.position.set( -5, 0, -12 );
+const blueLight2 = new THREE.PointLight(0x3366cc, 400, 2000);
+blueLight2.position.set(-5, 0, -12);
 
 camera.add(light1);
 camera.add(light2);
@@ -39,23 +39,23 @@ scene.add(camera);
 
 // Create renderer
 const renderer = new THREE.WebGLRenderer({ alpha: true });
-renderer.setSize( window.innerWidth, window.innerHeight );
-renderer.setClearColor( 0x000000, 0 );
-document.body.appendChild( renderer.domElement );
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setClearColor(0x000000, 0);
+document.body.appendChild(renderer.domElement);
 
 // Append the renderer to a section in the HTML file
-const section = document.getElementById( "hero-model" );
-section.appendChild( renderer.domElement );
+const section = document.getElementById("hero-model");
+section.appendChild(renderer.domElement);
 
 // Create controls
-const controls = new OrbitControls( camera, renderer.domElement );
+const controls = new OrbitControls(camera, renderer.domElement);
 controls.enablePan = false;
 controls.enableZoom = false;
 camera.position.z = 7;
 controls.update();
 
 let animationRunning = false;
-gsap.registerPlugin(MotionPathPlugin) 
+gsap.registerPlugin(MotionPathPlugin)
 
 function startAnimation() {
 	let currentX = camera.position.x;
@@ -74,22 +74,22 @@ function startAnimation() {
 
 	if (!animationRunning) {
 		animationRunning = true;
-		gsap.to(camera.position, { 
-    		duration: 3, 
+		gsap.to(camera.position, {
+			duration: 3,
 			motionPath: {
 				path: [
-					{x:currentX, y:currentY, z:currentZ}, 
-					{x:currentX + (newX - currentX) / 2, y:currentY, z:currentZ + (newZ - currentZ) / 2}, 
-					{x:newX, y:currentY/2, z:newZ}, 
-					{x: newX, y: 0, z: newZ}],
+					{ x: currentX, y: currentY, z: currentZ },
+					{ x: currentX + (newX - currentX) / 2, y: currentY, z: currentZ + (newZ - currentZ) / 2 },
+					{ x: newX, y: currentY / 2, z: newZ },
+					{ x: newX, y: 0, z: newZ }],
 				type: "cubic"
-			  },
-    		ease: "power2.inOut", 
-    		onComplete: () => {
-        		if (animationRunning) {
-            		startAnimation();
-        		}
-    		}
+			},
+			ease: "power2.inOut",
+			onComplete: () => {
+				if (animationRunning) {
+					startAnimation();
+				}
+			}
 		});
 	}
 }
@@ -99,26 +99,26 @@ function stopAnimation() {
 	gsap.killTweensOf(camera.position); // Stop the animation
 }
 
-const modelRotationZ= -0.003;
+let modelRotationZ = -0.003;
 
 // Listen for console commands
 let animationStartTimeout;
 let isCountingDown = false;
 
 controls.addEventListener('start', function () {
-    stopAnimation();
-    if (isCountingDown) {
-        clearTimeout(animationStartTimeout);
-        isCountingDown = false;
-    }
+	stopAnimation();
+	if (isCountingDown) {
+		clearTimeout(animationStartTimeout);
+		isCountingDown = false;
+	}
 });
 
 controls.addEventListener('end', function () {
-    if (!isCountingDown) {
+	if (!isCountingDown) {
 		isCountingDown = true;
-        animationStartTimeout = setTimeout(startAnimation, 1000);
-        modelRotationZ = -0.003;
-    }
+		animationStartTimeout = setTimeout(startAnimation, 1000);
+		modelRotationZ = -0.003;
+	}
 });
 
 
@@ -127,44 +127,44 @@ const loader = new GLTFLoader();
 
 // DRACO loader for compressed geometry
 const dracoLoader = new DRACOLoader();
-dracoLoader.setDecoderPath( 'https://www.gstatic.com/draco/versioned/decoders/1.5.7/' );
-dracoLoader.setDecoderConfig( { type: 'js' } );
-loader.setDRACOLoader( dracoLoader );
+dracoLoader.setDecoderPath('https://www.gstatic.com/draco/versioned/decoders/1.5.7/');
+dracoLoader.setDecoderConfig({ type: 'js' });
+loader.setDRACOLoader(dracoLoader);
 
 let model;
 
 // glTF loader
-loader.load('models/wavetag.glb', function ( gltf ) {
-	
-		model = gltf.scene;
-		model.rotation.x = Math.PI / 2;
-		scene.add( model );
+loader.load('models/wavetag.glb', function (gltf) {
 
-	},
+	model = gltf.scene;
+	model.rotation.x = Math.PI / 2;
+	scene.add(model);
+
+},
 	// loading progress
-	function ( xhr ) {
+	function (xhr) {
 
-		console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
+		console.log((xhr.loaded / xhr.total * 100) + '% loaded');
 
 	},
 	// loading error message
-	function ( error ) {
+	function (error) {
 
-		console.log( 'An error happened' );
+		console.log('An error happened');
 
 	}
 );
 
 // Animation loop
 function animate() {
-    requestAnimationFrame(animate);
+	requestAnimationFrame(animate);
 
 	controls.enableDamping = true;
-    controls.update();
+	controls.update();
 
 	model.rotation.z += modelRotationZ;
 
-    renderer.render(scene, camera);
+	renderer.render(scene, camera);
 }
 
 animate();
--- root/other.js
+++ root/other.js
@@ -1,8 +1,53 @@
-document.querySelector('#nav-logo').addEventListener('click', function(e) {
-    e.preventDefault(); // Prevent the default link behavior
-  
-    window.scrollTo({
-      top: 0,
-      behavior: 'smooth' // Enable smooth scrolling
-    });
-  });
+const setupParallax = document.getElementById('how-it-work-p');
+const whyUsParallaxMinus2 = document.getElementById('question-layer--2');
+const whyUsParallaxMinus1 = document.getElementById('question-layer--1');
+const whyUsParallaxPlus1 = document.getElementById('question-layer-1');
+const whyUsParallaxPlus2 = document.getElementById('question-layer-2');
+const whyUs = document.getElementById('why-us');
+
+window.addEventListener('scroll', function () {
+  let scrollPosition = window.scrollY;
+  let setupParallaxPosition = (scrollPosition - 1500) * (17 / 21) - 300;
+
+  if (scrollPosition < 1500) {
+    setupParallax.style.transform = `translateX(-50%) translateY(-300px)`;
+  } else if (scrollPosition > 3600) {
+    setupParallax.style.transform = `translateX(-50%) translateY(1400px)`;
+  } else {
+    setupParallax.style.transform = `translateX(-50%) translateY(${setupParallaxPosition}px)`;
+  }
+});
+
+window.addEventListener('scroll', function () {
+  let scrollPosition = window.scrollY;
+  let whyUsPosition = 0 - (scrollPosition - 4800)
+  let whyUsParallaxMinus2Position = 50 - (scrollPosition - 3400) * (1 / 15);
+  let whyUsParallaxMinus1Position = 100 - (scrollPosition - 3400) * (2 / 15);
+  let whyUsParallaxPlus1Position = (scrollPosition - 3400) * (3 / 15) - 150;
+  let whyUsParallaxPlus2Position = (scrollPosition - 3400) * (4 / 15) - 250;
+
+  if (scrollPosition < 3400) {
+    whyUsParallaxMinus2.style.transform = `translateX(-50.8%) translateY(50px)`;
+    whyUsParallaxMinus1.style.transform = `translateX(-50.8%) translateY(100px)`;
+    whyUsParallaxPlus1.style.transform = `translateX(-50.8%) translateY(-150px)`;
+    whyUsParallaxPlus2.style.transform = `translateX(-50.8%) translateY(-250px)`;
+  } else if (scrollPosition > 4900) {
+    whyUsParallaxMinus2.style.transform = `translateX(-50.8%) translateY(-50px)`;
+    whyUsParallaxMinus1.style.transform = `translateX(-50.8%) translateY(-100px)`;
+    whyUsParallaxPlus1.style.transform = `translateX(-50.8%) translateY(150px)`;
+    whyUsParallaxPlus2.style.transform = `translateX(-50.8%) translateY(150px)`;
+  } else {
+    whyUsParallaxMinus2.style.transform = `translateX(-50.8%) translateY(${whyUsParallaxMinus2Position}px)`;
+    whyUsParallaxMinus1.style.transform = `translateX(-50.8%) translateY(${whyUsParallaxMinus1Position}px)`;
+    whyUsParallaxPlus1.style.transform = `translateX(-50.8%) translateY(${whyUsParallaxPlus1Position}px)`;
+    whyUsParallaxPlus2.style.transform = `translateX(-50.8%) translateY(${whyUsParallaxPlus2Position}px)`;
+  }
+
+  if (scrollPosition < 4800) {
+    whyUs.style.transform = `translateY(0px)`;
+  } else if (scrollPosition > 5100) {
+    whyUs.style.transform = `translateY(-300px)`;
+  } else {
+    whyUs.style.transform = `translateY(${whyUsPosition}px)`;
+  }
+});
